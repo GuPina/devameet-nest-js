c5d8b1e1d880eb89feb0038e0256f78de7ec2db8
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-05-24 18:22:11 -0300

    aula 2 parte 2

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOND: "Login informado não é valido",
+    AUTH_PASSWORD_NOT_FOND: "Senha informada não é valida",
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOND: "E-mail e/ou senha não encontrados",
+}
