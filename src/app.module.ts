953430b1edea805464a8b9d16434bd2ac21a45f6
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-05-26 18:40:17 -0300

    aula 03 parte 02 - adicionamos o Guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './auth/user/user.module';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { UserModule } from './auth/user/user.module';
     UserModule
   ],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD, useClass: JwtAuthGuard}
+  ],
 })
 export class AppModule {}
