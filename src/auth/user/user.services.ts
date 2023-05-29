a09c2121324d683513b0f1b7fcef542e574d100e
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-05-29 17:29:56 -0300

    aula 03 parte 01

--- src/auth/user/user.services.ts
+++ src/auth/user/user.services.ts
@@ -4,6 +4,7 @@ import { User, UserDocument } from './schemas/use.schema';
 import { Model } from 'mongoose';
 import { RegisterDto } from './dtos/register.dto';
 import * as CryptoJS from 'crypto-js';
+import { UpdadeUserDto } from './dtos/updateuser.dt';
 
 @Injectable()
 export class UserService {
@@ -42,5 +43,9 @@ export class UserService {
 
     async getUserById(id:string){
         return await this.userModel.findById(id);
+    }
+
+    async updateUser(id: string, dto: UpdadeUserDto){
+       return await this.userModel.findByIdAndUpdate(id, dto); 
     }
 }
