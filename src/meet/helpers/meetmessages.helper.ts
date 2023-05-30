d2a09815d5c7ef794aefa19934efef6b5eb42da8
Gustavo Pina <115650988+GuPina@users.noreply.github.com>
2023-05-29 21:23:21 -0300

    aula 04 parte 03

--- src/meet/helpers/meetmessages.helper.ts
+++ src/meet/helpers/meetmessages.helper.ts
@@ -1,4 +1,9 @@
 export const MeetMessagesHelper = {
     CREATE_NAME_NOT_VALID: "Nome da reunião é inválido",
     CREATE_COLOR_NOT_VALID: "Cor da reunião é inválida, precisa ser um hexadecimal",
+    UPDATE_OBJECT_NAME_NOT_VALID: "Nome do objeto é invalido",
+    UPDATE_XY_NOT_VALID: "Eixo x e y do objeto deve ser entre 0 e 8",
+    UPDATE_ZINDEX_NOT_VALID: "Zindex é invalido",
+    UPDATE_ORIENTATION_NOT_VALID: "Orientação inválida",
+    UPDATE_MEET_NOT_FOUND: "Reunião não encontrada",
 }
--- src/meet/meet.service.ts
+++ src/meet/meet.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Meet, MeetDocument } from './schemas/meet.schema';
 import { Model } from 'mongoose';
@@ -6,6 +6,9 @@ import { UserService } from 'src/auth/user/user.services';
 import { GetMeetDto } from './dtos/getmeet.dto';
 import { CreateMeetDto } from './dtos/createmeet.dto';
 import { generateLink } from './helpers/linkgenerator.helper';
+import { MeetObject, MeetObjectDocument } from './schemas/meetobject.schema';
+import { UpdateMeetDto } from './dtos/updatemeet.dto';
+import { MeetMessagesHelper } from './helpers/meetmessages.helper';
 
 @Injectable()
 export class MeetService {
@@ -13,6 +16,7 @@ export class MeetService {
 
     constructor(
         @InjectModel(Meet.name) private readonly model: Model<MeetDocument>,
+        @InjectModel(MeetObject.name) private readonly ObjectModel: Model<MeetObjectDocument>,
         private readonly userService: UserService
     ){}
 
@@ -38,5 +42,41 @@ export class MeetService {
     async deleteMeetByUser(userId:String, meetId:string){
         this.logger.debug(`deleteMeetByUser - ${userId} - ${meetId}`);
         return await this.model.deleteOne({user: userId, _id: meetId});
+    }
+
+    async getMeetObjects(meetId:string, userId:string){
+        this.logger.debug(`deleteMeetByUser - ${userId} - ${meetId}`);
+        const user = await this.userService.getUserById(userId);
+        const meet = await this.model.findOne({user, _id: meetId});
+
+        return await this.ObjectModel.find({meet});
+    }
+
+    async update(meetId:string, userId:string, dto: UpdateMeetDto){
+        this.logger.debug(`update - ${userId} - ${meetId}`);
+        const user = await this.userService.getUserById(userId);
+        const meet = await this.model.findOne({user, _id: meetId});
+
+        if(!meet){
+            throw new BadRequestException(MeetMessagesHelper.UPDATE_MEET_NOT_FOUND)
+        }
+
+        meet.name = dto.name;
+        meet.color = dto.color;
+        await this.model.findByIdAndUpdate({_id: meetId}, meet);
+
+        await this.ObjectModel.deleteMany({meet});
+
+        let objectPayload;
+
+        for (const object of dto.objects) {
+            objectPayload = {
+                meet,
+                ...object
+            }
+
+            await this.ObjectModel.create(objectPayload);
+        }
+
     }
 }
